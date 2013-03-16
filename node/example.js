2d205a4cd8179b6f8653a23cfaf1c3afc3a58b5b
Denis  Seleznev <hcodes@yandex.ru>
2013-03-16 12:02:50 +0400

    Доработка методов

--- node/example.js
+++ node/example.js
@@ -1,4 +1,30 @@
 var YaMetrika = require('yametrika');
+    
+counter = new YaMetrika(123456); // номер счётчика Метрики
+counter.hit(); // Значение URL и referer берутся по умолчанию
 
-var counter = new YaMetrika(123456);
-counter.hit();
+// Отправка хита
+counter.hit('http://example.ru', 'Main page', 'http://ya.ru');
+counter.hit('/index.html', 'Main page', '/back.html');
+
+// Отправка хита вместе с пользовательскими параметрами
+counter.hit('http://example.ru', 'Main page', 'http://ya.ru', myParams);
+
+// Отправка хита вместе с параметрами визитов и с запретом на индексацию
+counter.hit('http://example.ru', 'Main page', 'http://ya.ru', myParams, 'noindex');
+
+// Достижение цели
+counter.reachGoal('back');
+
+// Внешняя ссылка - отчёт "Внешние ссылки"
+counter.extLink('http://yandex.ru');
+
+// Загрузка файла - отчёт "Загрузка файлов"
+counter.file('http://example.ru/file.zip');
+counter.file('/file.zip');
+
+// Отправка пользовательских параметров - отчёт "Параметры визитов"
+counter.params({level1: {level2: 1});
+
+// Не отказ
+counter.notBounce();
--- node/yametrika.js
+++ node/yametrika.js
@@ -1,9 +1,20 @@
-// TODO
+/*
+    http://webfilin.ru/notes/server_yametrika/
+    
+    Author: Seleznev Denis, hcodes@yandex.ru
+    Description: Серверная отправка хитов с помощью Node.js в Яндекс.Метрику
+    Version: 0.1
+    License: MIT
+*/
 
-var YaMetrika = function (counterId, counterClass, encoding) {
-    this.counterId = counterId;
-    this.counterClass = counterClass || 0;
-    this.encoding = encoding || 'utf-8';
+var querystring = require('querystring');
+var http = require('http');
+
+var YaMetrika = function (data) {
+    this._id = data.id;
+    this._type = data.type || 0;
+    this._encoding = data.encoding || 'utf-8';
+    this._host = data.host;
 };
 
 const HOST = 'mc.yandex.ru';
@@ -14,11 +25,31 @@ YaMetrika.prototype = {
     constructor: YaMetrika,
     // Отправка хита
     hit:  function (pageUrl, pageTitle, pageRef, userParams, ut) {
-        // TODO
+        if (!pageUrl) {
+            pageUrl = this.currentPageUrl();
+        }
+        
+        if (pageRef !== null) {
+            pageRef = this.getReferer();
+        }
+        
+        pageUrl = this.absoluteUrl(pageUrl, currentUrl);
+        pageRef = this.absoluteUrl(pageRef, currentUrl);
+
+        this.hitExt(pageUrl, pageTitle, pageRef, userParams, {ut: ut});
     },
     // Достижение цели
     reachGoal: function (target, userParams) {
-        // TODO
+        var referer;
+        if (target) {
+            target = 'goal://' + this.getHost() + '/' + target;
+            referer = this.currentPageUrl();
+        } else {
+            target = this.currentPageUrl();
+            referer = this.getReferer();
+        }
+        
+        this.hitExt(target, null, referer, userParams, null);
     },
     // Внешняя ссылка
     extLink: function (url, title) {
@@ -53,21 +84,94 @@ YaMetrika.prototype = {
     },
     // Общий метод для отправки хитов
     hitExt: function (pageUrl, pageTitle, pageRef, userParams, modes) {
-        // TODO
+        var postData = [];
+
+        if (this._type) {
+            postData['cnt-class'] = this._type;
+        }
+        
+        if (pageUrl) {
+            postData['page-url'] = encodeURI(pageUrl);
+        }
+        
+        if (pageRef) {
+            postData['page-ref'] = encodeURI(pageRef);
+        }         
+        
+        if (modes) {
+            modes['ar'] = true;
+        } else  {
+            modes = {ar: true};
+        }
+        
+        browserInfo = [];
+        for(var key in modes) {
+            if (!modes.hasOwnProperty(key)) {
+                continue;
+            }
+            
+            if (key != 'ut') {
+                browserInfo.push(key + ':' + (modes[key] === true ? 1 : modes[key]));
+            }
+        }
+        
+        browserInfo.push('en:' + this._encoding);
+
+        if (pageTitle) {
+            browserInfo.push('t:' + encodeURI(pageTitle));
+        }
+        
+        postData['browser-info'] = browserInfo.join(':');
+        
+        if (userParams) {
+            postData['site-info'] = encodeURI(JSON.stringify(userParams));
+        }
+
+        if (modes['ut']) {
+            postData['ut'] = modes['ut'];
+        }
+
+        var getQuery = PATH + this._id + '/1?rn=' + (Math.floor(Math.random() * 1E6)) + '&wmode=2';
+
+        this.postRequest(HOST, getQuery, querystring.stringify(postData));
     },
+    getHost: function () {
+        // TODO
+        
+        return this._host;
+    },
+    getReferer: function () {
+        // TODO
+        
+        return '';
+    },    
     // Текущий URL
     currentPageUrl: function () {
         // TODO
+        
+        return '';
     },
     // Преобразование из относительного в абсолютный url
     absoluteUrl: function (url, baseUrl) {
         // TODO
-    },
-    // Построение переменных в запросе
-    buildQueryVars: function (queryVars) {
-        // TODO
+        
+        return '';
     },
     postRequest: function (host, path, dataToSend) {
-        // TODO
+        var req = http.request({
+            host: HOST,
+            port: PORT,
+            method: 'POST',
+            path: path,
+            headers: {
+                'X-Real-IP': $_SERVER['REMOTE_ADDR'], // TODO
+                'User-Agent': $_SERVER['HTTP_USER_AGENT'] // TODO
+            },
+        }, function () {});
+        
+        req.write(dataToSend, this._encoding);
+        req.end();
     }
 };
+
+exports.yametrika = YaMetrika;
