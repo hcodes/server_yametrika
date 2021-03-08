b542cf0256751cab68774b0be8f58a9b4b1a50d3
Denis Seleznev <hcodes@yandex.ru>
2021-03-08 15:30:11 +0300

    Update example.php

--- examples/example.php
+++ examples/example.php
@@ -1,8 +1,16 @@
 <?php
 
-require_once('yametrika.php');
+require('../src/YaMetrika.php');
 
-$counter = new YaMetrika(123456);
+use ServerYaMetrika\YaMetrika;
+
+$counter = new YaMetrika(21312094);
 $counter->hit();
+$counter->hit('https://mysite.org', 'My site', 'https://yandex.ru');
+$counter->reachGoal('goal_name');
+$counter->notBounce();
+$counter->file('https://mysite.org/file.zip', 'File title');
+$counter->extLink('https://yandex.ru', 'Yandex');
+$counter->params(['Param1' => 1, 'Param2' => 'value']);
 
 ?>
